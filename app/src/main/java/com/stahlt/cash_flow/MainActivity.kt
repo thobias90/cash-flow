f549d9c0b8b2ad090205aaf7507f7d1e000c37af
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-17 12:29:07 -0300

    feat: add spinner example

--- app/src/main/java/com/stahlt/cash_flow/MainActivity.kt
+++ app/src/main/java/com/stahlt/cash_flow/MainActivity.kt
@@ -2,10 +2,33 @@ package com.stahlt.cash_flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var spinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spinner = findViewById(R.id.spinner)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.cash_options,
+            android.R.layout.simple_spinner_item)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+
+        spinner.adapter = adapter
+
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.cash_options,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//                spinner.adapter = adapter
+//        }
     }
 }
