6f55d95b976e8ffee3fa92226b706c1f827ce051
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-17 13:49:46 -0300

    feat: add ui

--- app/src/main/java/com/stahlt/cash_flow/MainActivity.kt
+++ app/src/main/java/com/stahlt/cash_flow/MainActivity.kt
@@ -2,6 +2,9 @@ package com.stahlt.cash_flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
@@ -11,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        spinner = findViewById(R.id.spinner)
+        spinner = findViewById(R.id.cashType)
 
         val adapter = ArrayAdapter.createFromResource(
             this,
@@ -22,13 +25,14 @@ class MainActivity : AppCompatActivity() {
 
         spinner.adapter = adapter
 
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.cash_options,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//                spinner.adapter = adapter
-//        }
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Log.d("..::MY APP", "onItemSelected")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                Log.d("..::MY APP", "onNothingSelected")
+            }
+        }
     }
 }
